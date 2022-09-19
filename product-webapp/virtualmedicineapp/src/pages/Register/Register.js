3b90953c28f637fc985fd0e91804ecd2b05545bd
Nehalnarnaware <111412132+Nehalnarnaware@users.noreply.github.com>
2022-09-19 12:08:26 +0530

    Update Register.js

--- product-webapp/virtualmedicineapp/src/pages/Register/Register.js
+++ product-webapp/virtualmedicineapp/src/pages/Register/Register.js
@@ -4,7 +4,7 @@ import registerImg from '../../assets/register.jpg'
 import axios from 'axios';
 import { useNavigate,Link } from "react-router-dom";
 
-const host = 'https://virtualmedicine.stackroute.io';
+const host = 'https://54.71.221.112:8080';
 
 function Register() {
     const [formData, setformData] = useState({role:'patient',email:'',password:'',passwordConfirm:''});
